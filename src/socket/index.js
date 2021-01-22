3852dad5446b86d9194958194bb4f3aa9b163058
Eddy <eddy@gocollo.com>
2021-01-21 22:21:40 -0500

    add loading display to join and create game

--- src/socket/index.js
+++ src/socket/index.js
@@ -2,4 +2,4 @@ import io from 'socket.io-client'
 
 const SOCKET_URL = 'https://imposters-app.herokuapp.com/'
 
-export const socket = io(SOCKET_URL)
+export const socket = io(SOCKET_URL, { transports: ['websocket', 'polling']})
